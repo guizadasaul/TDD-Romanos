d336ba16a8f35d27eeae532821b7f95c5670133f
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-03-04 14:14:16 -0400

    feat: calcula el resultado hasta el numero 10

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,9 @@
 function convertToRoman(num) {
   const romanNumerals = [
-
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
     { value: 1, numeral: 'I' },
   ];
 
--- src/converter.test.js
+++ src/converter.test.js
@@ -2,12 +2,25 @@ import convertToRoman from "./converter.js";
 
 
 describe("Converter", () => {
-    
     it("Debe retornar I", () => {
       const resultado = convertToRoman(1);
       expect(resultado).toBe("I");
     });
+
+    it("Debe retornar VII", () => {
+        const resultado = convertToRoman(7);
+        expect(resultado).toBe("VII");
+      });
+
+    it("Debe retornar III", () => {
+        const resultado = convertToRoman(3);
+        expect(resultado).toBe("III");
+      });
     
+    it("Debe retornar X", () => {
+        const resultado = convertToRoman(10);
+        expect(resultado).toBe("X");
+      });
 });
   
   
